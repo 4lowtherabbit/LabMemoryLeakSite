c3bb67ad7ab3930426a222e65d1fa83ff554e9fb
Juntao Zhu <juzhu@microsoft.com>
2019-03-08 17:44:29 +0800

    init

---
+++ LabMemoryLeak/Controllers/ReproController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LabMemoryLeak.Controllers
+{
+    public class ReproController : Controller
+    {
+        static volatile int id = 0;
+        static HttpClient client = new HttpClient();
+        public async Task<ActionResult> Index()
+        {
+            string schema = Request.IsSecureConnection ? "https": "http";
+            string serverName = Request.ServerVariables["server_name"];
+            string serverPort = Request.ServerVariables["server_port"];
+            string path = Request.ApplicationPath;
+            string urlString = $"{schema}://{serverName}:{serverPort}{path}Repro/Cached/{id++}";
+            string response = await client.GetStringAsync(urlString);
+
+            ViewData["URL"] = urlString;
+            ViewData["Response"] = response;
+            return View();
+        }
+
+        [OutputCache(Duration = 3*24*60*60, VaryByParam = "id")]
+        public ActionResult Cached(int id)
+        {
+            ViewBag.Id = id;
+            return View();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Lab of a high Memory App Service site
+[![Deploy to Azure](http://azuredeploy.net/deploybutton.png)](https://azuredeploy.net/)
